c2d92ee260a407f21e6d0210d98cee3378cde856
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-06-05 09:35:15 -0700

    set up home index view and controller

--- PierresTreats/Controllers/HomeController.cs
+++ PierresTreats/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Library.Controllers
 {
@@ -7,7 +8,13 @@ namespace Library.Controllers
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      Dictionary<string, object> everything = new Dictionary<string, object>();
+      var allBooks = _db.Books.ToList();
+      var allFlavors = _db.Flavors.ToList();
+      everything.Add("books", allBooks);
+      everything.Add("flavors", allFlavors);
+
+      return View(everything);
     }
   }
 }
