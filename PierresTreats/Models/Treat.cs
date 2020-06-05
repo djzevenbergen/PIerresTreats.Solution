9581ec02a382f318472f2aa9d4c6fcfde23150f9
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-06-05 09:12:15 -0700

    add treat model

---
+++ PierresTreats/Models/Treat.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace PierresTreats.Models
+{
+  public class Treat
+  {
+    public Treat()
+    {
+      this.Flavors = new HashSet<FlavorTreat>();
+    }
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public virtual ApplicationUser User { get; set; }
+    public ICollection<FlavorTreat> Flavors { get; set; }
+  }
+}
