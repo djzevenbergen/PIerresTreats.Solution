573378a9cc83ad84486919abfbf200199a561eb2
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-06-06 12:34:42 -0700

    attempt to get working on my other computer

---
+++ PierresTreats/Migrations/20200606191937_Final.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PierresTreats.Migrations
+{
+    public partial class Final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Treats");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Flavors");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Treats",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Flavors",
+                nullable: true);
+        }
+    }
+}
